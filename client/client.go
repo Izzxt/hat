9b7f47aecaf2da07c4b8d4dc7ee5bf771156fd17
Izzxt <haziqizzat89@gmail.com>
2022-09-09 21:48:31 +0800

    Using NewRequest instead

--- client/client.go
+++ client/client.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -17,8 +18,7 @@ type Client struct {
 func NewClient() Client {
 	var DefaultTransport http.RoundTripper = &http.Transport{
 		DialContext: (&net.Dialer{
-			Timeout:   30 * time.Second,
-			KeepAlive: 30 * time.Second,
+			KeepAlive: 24 * time.Second,
 		}).DialContext,
 		MaxIdleConns:    100,
 		IdleConnTimeout: 90 * time.Second,
@@ -37,10 +37,26 @@ func NewClient() Client {
 
 func (c *Client) Get(fetchUrl string) *http.Response {
 
-	response, err := c.client.Get(fetchUrl)
+	req, err := http.NewRequest("GET", fetchUrl, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	response, err := c.client.Do(req)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	return response
+}
+
+func Exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
 }
