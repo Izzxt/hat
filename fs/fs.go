39772efa0e886622db75c7a9a4865e22b545f148
Izzxt <haziqizzat89@gmail.com>
2022-09-09 21:51:48 +0800

    Add exists function

---
+++ fs/fs.go
@@ -0,0 +1,16 @@
+package fs
+
+import (
+	"os"
+)
+
+func Exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
