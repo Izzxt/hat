51a764bc9cb7bf890bdbf4b5ea08b8fd2b4866a9
Izzxt <haziqizzat89@gmail.com>
2022-09-10 20:29:52 +0800

    Update regex and GetAllCode

--- badges/badges.go
+++ badges/badges.go
@@ -1,10 +1,13 @@
 package badges
 
 import (
+	"fmt"
+	"log"
 	"regexp"
 	"sync"
 
 	"github.com/Izzxt/hat/downloader"
+	"github.com/Izzxt/hat/fs"
 )
 
 type Badges struct {
@@ -21,7 +24,8 @@ func NewBadges(downloader downloader.Downloader, wg *sync.WaitGroup, mu *sync.Mu
 	}
 }
 
-func (b *Badges) GetAllCode() {
+func (b *Badges) GetAllCode() []string {
+	var code []string
 	dw := b.downloader
 
 	domain := []string{
@@ -30,18 +34,30 @@ func (b *Badges) GetAllCode() {
 		"fr", "it", "nl",
 	}
 
-	for _, v := range domain {
-		defer b.wg.Done()
-		go func(v string) {
-			dw.SetDomain(v)
-			dw.SetPath("/gamedata/external_flash_texts/0")
-		}(v)
+	for _, d := range domain {
+		dw.SetDomain(d)
+		dw.SetPath("/external_flash_texts/0")
+		fmt.Println(dw.GetUrl())
+		byte, _ := dw.Fetch()
+		match := matchRegex(string(byte))
+		for _, v := range match {
+			exts, err := fs.Exists(fmt.Sprintf("%s%s%s", dw.GetOutput(), v[2], ".gif"))
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if !exts {
+				code = append(code, v[2])
+			}
+		}
 	}
+
+	return code
 }
 
 func matchRegex(findString string) [][]string {
 
-	m := regexp.MustCompile(`(?m)badge_(desc|name)_*(.[\w+\-$#!@#$%^&*()].*)=`)
+	m := regexp.MustCompile(`(?m)badge_(desc|name)_*(.[\w+\-$#!@#$%^&*()\s.|]*)=`)
 
 	return m.FindAllStringSubmatch(findString, -1)
 }
