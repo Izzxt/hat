011be0da16b13bf5ec90e2c43beb2975967d5b11
Izzxt <haziqizzat89@gmail.com>
2022-09-11 21:08:23 +0800

    Fix unable to find file

--- badges/badges_test.go
+++ badges/badges_test.go
@@ -21,7 +21,7 @@ func TestDownloadBadge(t *testing.T) {
 	c := client.NewClient()
 	d := downloader.NewDownloader(c)
 	d.SetOther()
-	d.SetOutput("")
+	d.SetOutput("out")
 	d.SetDomain("com")
 	d.SetPath("/c_images/album1584")
 
@@ -30,9 +30,9 @@ func TestDownloadBadge(t *testing.T) {
 		d.Download()
 	}
 
-	assert.FileExists(fmt.Sprintf("%s.gif", code[0]))
-	assert.FileExists(fmt.Sprintf("%s.gif", code[1]))
-	assert.FileExists(fmt.Sprintf("%s.gif", code[2]))
+	assert.FileExists(fmt.Sprintf("out/%s.gif", code[0]))
+	assert.FileExists(fmt.Sprintf("out/%s.gif", code[1]))
+	assert.FileExists(fmt.Sprintf("out/%s.gif", code[2]))
 }
 
 func TestGetAllCode(t *testing.T) {
