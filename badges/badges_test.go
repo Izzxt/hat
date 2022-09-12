f9926a5f094044a271c324d9555deba7a48edaf1
Izzxt <haziqizzat89@gmail.com>
2022-09-12 15:52:56 +0800

    Fix test failed

--- badges/badges_test.go
+++ badges/badges_test.go
@@ -52,19 +52,21 @@ func TestGetAllCode(t *testing.T) {
 func TestMatchRegex(t *testing.T) {
 	assert := assert.New(t)
 	match := `
-		badge_desc_JKFES FKES=Hello World
-		badge_desc_JK|FES_FKES=Hello World
-		badge_desc_JKFES-FKES!0=Hello World
-		badge_desc_JKFES+FKES_0.=Hello World
-		badge_desc_JKFES(FKES__=Hello World
-		badge_name_JK-$FES_FKES=Hello World
-		badge_name_JKFES-FKES!0=Hello World
-		badge_name_$#JKFES+FKES_0.=Hello World
-		badge_name_@#JKFES(FKES__=Hello World
-		badge_name_()JKFES FKES_=Hello World
+badge_desc_JKFES FKES=Hello World
+badge_desc_JK|FES_FKES=Hello World
+badge_desc_JKFES-FKES!0=Hello World
+badge_desc_JKFES+FKES_0.=Hello World
+badge_desc_JKFES(FKES__=Hello World
+badge_name_JK-$FES_FKES=Hello World
+badge_name_JKFES-FKES!0=Hello World
+badge_name_$#JKFES+FKES_0.=Hello World
+badge_name_@#JKFES(FKES__=Hello World
+badge_name_()JKFES FKES_=Hello World
 	`
 
 	m := matchRegex(match)
+
+	fmt.Print(m)
 
 	assert.Equal("JKFES FKES", m[0][2])
 	assert.Equal("JK|FES_FKES", m[1][2])
