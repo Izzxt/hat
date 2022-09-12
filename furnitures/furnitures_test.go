ddee98135e03f4e0ece5e011005ad7f0a3e9fad2
Izzxt <haziqizzat89@gmail.com>
2022-09-12 20:32:12 +0800

    Add furnis test unit

--- furnitures/furnitures_test.go
+++ furnitures/furnitures_test.go
@@ -22,3 +22,17 @@ func TestGetFurniIcons(t *testing.T) {
 	assert.Equal("shelves_norja_icon.png", i[0].Name)
 	assert.Equal("61856", i[0].Revision)
 }
+
+func TestGetFurnis(t *testing.T) {
+	assert := assert.New(t)
+	c := client.NewClient()
+	d := downloader.NewDownloader(c)
+	d.SetDomain("com")
+
+	f := NewFurnitures(*d, nil, nil)
+	i := f.GetFurnis()
+
+	fmt.Print(len(i))
+	assert.Equal("shelves_norja.swf", i[0].Name)
+	assert.Equal("61856", i[0].Revision)
+}
